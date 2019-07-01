4ee6d3ba1e04268083c681ea1faefb87e98e6e7f
Aldo Monteiro <aldomonteiro@gmail.com>
2019-07-01 17:03:08 -0300

    pages for all posts

--- gatsby/pagination/create-posts-pages.js
+++ gatsby/pagination/create-posts-pages.js
@@ -19,7 +19,10 @@ module.exports = async (graphql, actions) => {
   const languages = result.data.allMarkdownRemark.group;
   const { postsPerPage } = siteConfig;
 
+  let grandTotal = 0;
   languages.map((language) => {
+    grandTotal += language.totalCount;
+
     const numPages = Math.ceil(language.totalCount / postsPerPage);
 
     for (let i = 0; i < numPages; i += 1) {
@@ -40,4 +43,26 @@ module.exports = async (graphql, actions) => {
       });
     }
   });
+
+  /**
+   * Page for all posts all languages
+   */
+  const numPages = Math.ceil(grandTotal / postsPerPage);
+
+  for (let i = 0; i < numPages; i += 1) {
+    createPage({
+      path: i === 0 ? '/' : `page/${i}`,
+      component: path.resolve('./src/templates/index-template.js'),
+      context: {
+        currentPage: i,
+        postsLimit: postsPerPage,
+        postsOffset: i * postsPerPage,
+        prevPagePath: i <= 1 ? '/' : `page/${i - 1}`,
+        nextPagePath: `page/${i + 1}`,
+        hasPrevPage: i !== 0,
+        hasNextPage: i !== numPages - 1
+      }
+    });
+  }
+
 };
